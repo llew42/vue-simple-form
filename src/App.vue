686712232398b12259a94d5cd32ceed009e8c76b
Justin Llewellyn <jllew24@gmail.com>
2022-06-12 14:46:22 -0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <li>
+        <friend-contact
+          v-for="friend in friends"
+          :key="friend.id"
+          :id="friend.id"
+          :name="friend.name"
+          :phone-number="friend.phone"
+          :email-address="friend.email"
+          :is-favorite="friend.isFavorite"
+          @toggle-favorite="toggleFavoriteStatus"
+        >
+        </friend-contact>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+          isFavorite: true,
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+          isFavorite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const favoriteFriend = this.friends.find(friend => friend.id === friendId);
+      favoriteFriend.isFavorite = !favoriteFriend.isFavorite;
+    }
+  }
+};
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,130 @@
+<template>
+	<section>
+		<li>
+			<h2>{{ name }} {{ isFavorite ? 'Favorite' : '' }}</h2>
+			<button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+			<button @click="toggleFavorite">Favorite</button>
+		</li>
+		<ul v-if="detailsAreVisible">
+			<li><strong>Phone:</strong> {{ phoneNumber }}</li>
+			<li><strong>Email:</strong> {{ emailAddress }}</li>
+		</ul>
+	</section>
+</template>
+
+<script>
+export default {
+	props: {
+		id: {
+			type: String,
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		phoneNumber: {
+			type: String,
+			required: true,
+		},
+		emailAddress: {
+			type: String,
+			required: true,
+		},
+		isFavorite: {
+			type: Boolean,
+			required: false,
+			default: false,
+			// validator: (val) => {
+			// 	return val === '1' | val === '0'
+			// }
+		},
+	},
+	data() {
+		return {
+			detailsAreVisible: false,
+			friend: {
+				id: "manuel",
+				name: "Manuel Lorenz",
+				phone: "0123 45678 91",
+				email: "manuel@localhost.com",
+			},
+		};
+	},
+	methods: {
+		toggleDetails() {
+			this.detailsAreVisible = !this.detailsAreVisible;
+		},
+		toggleFavorite() {
+				this.$emit('toggle-favorite', this.id);
+		} 
+	}
+};
+</script>
+
+<style>
+	* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+</style>
